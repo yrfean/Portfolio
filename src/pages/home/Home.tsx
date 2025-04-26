99e99d3251e3d846b82f76900b323c79751c9fbb
unknown <irfanshalu3@gmail.com>
2025-04-26 22:11:32 +0530

    add role

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,10 +2,11 @@ const Home = () => {
   return (
     <div className="w-screen h-full flex items-center justify-center">
       <h1 className="text-2xl text-gray-400 font-bold">
-        Hai,iam <span className="text-gray-700">Irfan Ashraf</span>{" "}
+        Hi,iam <span className="text-gray-700">Irfan Ashraf</span>{" "}
       </h1>
+      <p>Software developer</p>
     </div>
   );
 }
 
-export default Home
+export default Home;
