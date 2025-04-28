718132ba524e56d1b94929b8336f79bc54386b3a
unknown <irfanshalu3@gmail.com>
2025-04-28 22:17:33 +0530

    role creagting

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const roles = [
   "Software Developer",
   "Fullstack Developer",
@@ -6,13 +8,22 @@ const roles = [
 ];
 
 const Home = () => {
+
+  const [role,setRole] = useState("Software developer")
+
+  useEffect(()=>{
+setInterval(()=>{
+// setRole() instead use index and change it in by time and update value in p tage
+},1000)
+  },[role])
+
   return (
     <div className="w-screen h-full flex items-center justify-center">
       <h1 className="text-2xl text-gray-400 font-bold">
         Hi,iam <span className="text-gray-700">Irfan Ashraf</span>{" "}
       </h1>
-      <p>Software developer</p>
-    </div>
+       <p></p>
+     </div>
   );
 };
 
